ecb8b2ebf616e951c8ab55422f7698324feb8544
Hongyu Lu <sannrei@hotmail.com>
2023-09-18 04:24:11 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import {Heading} from '@chakra-ui/react'
+import {Heading, Grid,GridItem,Show} from '@chakra-ui/react'
 
 function App() {
 
   return (
     
-   <Heading >Hello, you</Heading>
+   <Grid templateAreas={{
+    base: `'main nav'`,
+    sm: `'intro main nav'`,
+
+   }}>
+     <Show above='sm'>
+     <GridItem area='intro' bgColor={'pink'}>Intro</GridItem>
+     </Show>
+    
+    <GridItem area='main' bgColor={'gold'}>main</GridItem>
+    <GridItem area='nav' bgColor={'blue'}>nav</GridItem>
+
+
+   </Grid>
    
   )
 }
